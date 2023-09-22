c1d28aaf59ced955c22cad09a62b590708ea5ee8
Zappala Cristian <34014255+CristianIZ@users.noreply.github.com>
2023-09-22 00:53:36 -0300

    facade

---
+++ CZJB.FacadeTP/CZJB.Services/Facade/FacadeHospital.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CZJB.Services
+{
+    public class FacadeHospital
+    {
+        public void InscribirPaciente()
+        {
+
+        }
+
+        public void LlamarPaciente()
+        {
+
+        }
+
+        public void VacunarPaciente()
+        {
+
+        }
+    }
+}
